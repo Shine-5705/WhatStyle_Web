c0d30d227c225993b76dbb7c943cf2066f948451
Parth Sinha <sinhaparth555@gmail.com>
2025-08-02 17:33:16 +0100

    added config for seo

---
+++ src/config/seo.ts
@@ -0,0 +1,9 @@
+export const SEO = {
+  SITE_NAME: 'WhatStyle',
+  SITE_URL: 'https://whatstyle.com',
+  DEFAULT_TITLE: 'WhatStyle - Auto Reply AI Bot',
+  DEFAULT_DESC: 'Reply to the people automatically with our bot on WhatsApp',
+  DEFAULT_IMAGE: '/og-default.png',
+  TWITTER: '@shine_gupta17',
+  FACEBOOK_APP_ID: 'facebook_app_id'
+} as const;
