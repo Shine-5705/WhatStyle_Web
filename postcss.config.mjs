5969b0689884c5e43339a4b646157a4f28863df9
Parth Sinha <sinhaparth555@gmail.com>
2025-08-03 18:45:47 +0100

    updated package file

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  // plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    '@tailwindcss/postcss': {}, // Tailwind CSS plugin
+    autoprefixer: {}, // Adds vendor prefixes for browser compatibility
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}), // Minifies CSS only in production
+  },
 };
 
 export default config;
